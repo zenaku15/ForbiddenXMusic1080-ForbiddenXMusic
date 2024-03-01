51f4f76a019f9f2ab33765c77635c9eee9f89d40
zenaku15 <161702193+zenaku15@users.noreply.github.com>
2024-03-01 07:03:01 +0630

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,7 +1,19 @@
-from .Apple import AppleAPI
-from .Carbon import CarbonAPI
-from .Resso import RessoAPI
-from .Soundcloud import SoundAPI
-from .Spotify import SpotifyAPI
-from .Telegram import TeleAPI
-from .Youtube import YouTubeAPI
+import glob
+from os.path import dirname, isfile
+
+
+def __list_all_modules():
+    work_dir = dirname(__file__)
+    mod_paths = glob.glob(work_dir + "/*/*.py")
+
+    all_modules = [
+        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["ALL_MODULES"]
